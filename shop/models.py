3da52ac9e08b5d7eece7b0e9e08820f9eafd49bc
isrodick <isrodick@gmail.com>
2016-11-04 00:05:32 +0200

    create Product, Order and OrderProduct models

---
+++ shop/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import (
+	ForeignKey,
+	Column,
+	Integer,
+	Numeric,
+	Unicode,
+)
+from sqlalchemy.orm import relation
+
+from shop.database import Base
+
+
+class Product(Base):
+	__tablename__ = 'product'
+
+	id = Column(Integer, primary_key=True)
+	title = Column(Unicode(255), nullable=False)
+	price = Column(Numeric(precision=12, scale=4), nullable=False)
+	image_url = Column(Unicode(255))
+	qty = Column(Integer, nullable=False)
+
+
+class Order(Base):
+	__tablename__ = 'order'
+
+	id = Column(Integer, primary_key=True)
+	status = Column(Integer, nullable=False)
+
+	products = relation('OrderProduct', backref='order', lazy='subquery', cascade='all, delete-orphan')
+
+
+class OrderProduct(Base):
+	__tablename__ = 'order_product'
+
+	order_id = Column(Integer, ForeignKey('order.id', onupdate='CASCADE', ondelete='CASCADE'), index=True, primary_key=True)
+	product_id = Column(Integer, ForeignKey('product.id', onupdate='CASCADE', ondelete='CASCADE'), index=True, primary_key=True)
+	qty = Column(Integer, nullable=False)
+
+	product = relation(Product)
