8f3389f63789d8306fb5d401ee4e3a3a3cb3adf3
isrodick <isrodick@gmail.com>
2016-11-04 02:13:29 +0200

    create product_new controller

--- shop/controllers.py
+++ shop/controllers.py
@@ -1,8 +1,39 @@
-from flask import render_template
+from flask import (
+	request,
+	redirect,
+	url_for,
+    render_template,
+    flash,
+)
 
 from shop import app
 from shop.database import DBSession
-from shop.models import Product
+from shop.models import (
+    Product,
+    Order,
+    OrderProduct,
+)
+
+
+@app.route('/product/new', method=['GET', 'POST'])
+def product_new():
+	if request.method == 'POST':
+		product = Product()
+
+		product.title = request.form['title']
+		product.prie = request.form['prie']
+		product.image_url = request.form['image_url']
+		product.qty = request.form['qty']
+
+		DBSession.add(product)
+		DBSession.flush()
+		DBSession.commit()
+
+		flash('Product was created successfully')
+
+		return redirect(url_for('product_edit', id=product.id))
+
+	return render_template('product_new.html')
 
 
 @app.route('/')
