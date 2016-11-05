fcb532528bc0bac61bf8bdd3977aa67ab4032b46
isrodick <isrodick@gmail.com>
2016-11-05 20:06:48 +0200

    create _Enum class Enums are descendants of the _Enum

--- shop/models.py
+++ shop/models.py
@@ -24,12 +24,18 @@ def check_qty_positive(tablename):
 	return CheckConstraint('qty >= 0', name='{}_check_qty_positive'.format(tablename))
 
 
-class OrderStatus(enum.Enum):
+class _Enum(enum.Enum):
+	@classmethod
+	def get_options(cls):
+		return [(key, cls._member_map_[key].value) for key in cls._member_map_]
+
+
+class OrderStatus(_Enum):
 	new = 'New'
 	paid = 'Paid'
 
 
-class PaymentMethod(enum.Enum):
+class PaymentMethod(_Enum):
 	liqpay = 'LiqPay'
 	privat24 = 'Privat24'
 	paypal = 'PayPal'
