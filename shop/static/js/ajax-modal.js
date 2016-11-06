0c38be88d151f899d8aea6828e1cf87e9a388e8f
isrodick <isrodick@gmail.com>
2016-11-06 20:10:37 +0200

    use global HELPER object

--- shop/static/js/ajax-modal.js
+++ shop/static/js/ajax-modal.js
@@ -1,28 +1,4 @@
 $(function() {
-	function render_flash_message(message, error) {
-		var $flash_message_block = $('.flash-message-block'),
-			_class = (error == 'error')?  'alert-danger':'alert-warning';
-
-		var $span = $('<span/>'),
-			$span = $span.attr('aria-hidden', 'true'),
-			$span = $span.html('×');
-
-		var $close_btn = $('<button/>'),
-			$close_btn = $close_btn.attr('type', 'button'),
-			$close_btn = $close_btn.addClass('close'),
-			$close_btn = $close_btn.attr('data-dismiss', 'alert').data('dismiss', 'alert'),
-			$close_btn = $close_btn.attr('aria-label', 'Close'),
-			$close_btn = $close_btn.html($span);
-
-		var $div = $('<div/>'),
-			$div = $div.addClass('alert').addClass(_class),
-			$div = $div.attr('role', 'alert'),
-			$div = $div.html($close_btn),
-			$div = $div.append(message);
-
-		$flash_message_block.append($div);
-	};
-
 	$("#confirm-delete").on('click', '.btn-ok', function(ev) {
 		var $flash_message_block = $('.flash-message-block'),
 			$modal = $(this).closest('.modal'),
@@ -47,12 +23,12 @@ $(function() {
 			}
 
 			if (message) {
-				render_flash_message(message, _status);
+				HELPER.render_flash_message(message, _status);
 			}
 
 			$modal.modal('hide');
 		}).fail(function(data, status, xhr) {
-			render_flash_message(status, 'error');
+			HELPER.render_flash_message(status, 'error');
 
 			$modal.modal('hide');
 
