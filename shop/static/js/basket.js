d56d15662a6c3b61170a889adfe92b2f5ccc3e6d
isrodick <isrodick@gmail.com>
2016-11-07 22:49:33 +0200

    dynamic update the total price

--- shop/static/js/basket.js
+++ shop/static/js/basket.js
@@ -1,4 +1,6 @@
 $(function() {
+	var $total_price = $('span#total-price');
+
 	function render_error($form_group, $errors_row, message) {
 		var $error_p = $('<p/>'),
 			$error_p = $error_p.addClass('error'),
@@ -62,6 +64,10 @@ $(function() {
 
 				if (data.total_products_qty) {
 					HELPER.update_basket_total_qty(data.total_products_qty);
+				}
+
+				if ('total_price' in data) {
+					$total_price.html(data.total_price);
 				}
 			}
 		}).fail(function(data, status, xhr) {
