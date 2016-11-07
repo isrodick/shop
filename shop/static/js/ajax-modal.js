f4ee4a5ce4768a66fdabd2260d68901c8d3b6ab2
isrodick <isrodick@gmail.com>
2016-11-08 01:20:39 +0200

    a bit refactoring add trigger on the btn

--- shop/static/js/ajax-modal.js
+++ shop/static/js/ajax-modal.js
@@ -1,5 +1,5 @@
 $(function() {
-	$("#confirm-delete").on('click', '.btn-ok', function(ev) {
+	$(".confirm-delete").on('click', '.btn-ok', function(ev) {
 		var $flash_message_block = $('.flash-message-block'),
 			$modal = $(this).closest('.modal'),
 			$delete_btn = $('[data-target="#' + $modal.attr('id') + '"]'),
@@ -20,6 +20,9 @@ $(function() {
 
 			if (_status == 'error') {
 				message = message || 'Error';
+				$delete_btn.trigger('delete.error', data);
+			} else {
+				$delete_btn.trigger('delete.success', data);
 			}
 
 			if (message) {
@@ -31,6 +34,8 @@ $(function() {
 			HELPER.render_flash_message(status, 'error');
 
 			$modal.modal('hide');
+
+			$delete_btn.trigger('delete.error', data);
 
 			return;
 		});
